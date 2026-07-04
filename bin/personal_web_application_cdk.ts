8d9f3e0cddd879370e3812a45a42352cbd8af37c
BhargavHemanth Maganti <bhargavmaganti01@gmail.com>
2026-07-03 21:45:55 -0700

    Fix regions Add-multistage-pipeline

--- bin/personal_web_application_cdk.ts
+++ bin/personal_web_application_cdk.ts
@@ -4,14 +4,12 @@ import { PersistenceStack } from '../lib/stacks/persistence-stack';
 
 const app = new cdk.App();
 
-// Beta environment
 new PersistenceStack(app, 'Beta-PersistenceStack', {
-    env: { account: '933500793398', region: 'us-east-1' },
+    env: { account: '933500793398', region: 'us-west-2' },   // was us-east-1
     stageName: 'beta',
 });
 
-// Prod environment
 new PersistenceStack(app, 'Prod-PersistenceStack', {
-    env: { account: '933500793398', region: 'us-west-2' },
+    env: { account: '933500793398', region: 'us-east-1' },   // was us-west-2
     stageName: 'prod',
 });
