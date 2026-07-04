fd8d704dfb0433b553febc539101e61583c4945d
BhargavHemanth Maganti <bhargavmaganti01@gmail.com>
2026-07-03 20:38:53 -0700

    Initial CDK setup for PersonalWebApplicationCDK"

---
+++ test/personal_web_application_cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as PersonalWebApplicationCdk from '../lib/personal_web_application_cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/personal_web_application_cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new PersonalWebApplicationCdk.PersonalWebApplicationCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
